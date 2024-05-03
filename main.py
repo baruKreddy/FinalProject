d713fe7994b101c754d6a1fac3be95b64ac566dc
Bhargavi K Reddy <bhargavirkotha@gmail.com>
2024-05-02 21:18:45 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Depends
-from init_db import get_db_conn  # Ensure this function is properly defined to yield a database connection
-from routers.customer_router import router as customer_router
-from routers.item_router import router as item_router
-from routers.order_router import router as order_router
-app = FastAPI()
-app.include_router(customer_router)
-app.include_router(item_router)
-app.include_router(order_router)
